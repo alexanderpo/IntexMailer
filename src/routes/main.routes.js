4b3c10822bf3ca5e5f13eae353a2966b4c8dc7e2
alexanderpo <alexanderpometko@gmail.com>
2016-03-22 22:01:55 +0300

    Refractoring
    edit basic script whos change email template

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -4,21 +4,21 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('index', { title: 'Intex Mailer' });
+  res.render('index', { title: 'Intex Mailer' });
 });
 
 /* GET templates page. */
 router.get('/templates', (req, res) => {
-	res.render('templates', { title: 'Intex Mailer-Templates' });
+  res.render('templates', { title: 'Intex Mailer-Templates' });
 });
 
 router.get('/templates/:id', (req, res) => {
-	res.render('setuptemplate', { title: 'Intex Mailer - ' + req.params.id });
+  res.render('setuptemplate', { title: req.params.id });
 });
 
 /* GET contact page. */
 router.get('/contact', (req, res) => {
-	res.render('contact', { title: 'Intex Mailer-We Here' });
+  res.render('contact', { title: 'Intex Mailer-We Here' });
 });
 // Exporting an object as the default import for this module
 export default router;
